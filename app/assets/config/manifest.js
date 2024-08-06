c2f8deb1148e28815712c5f945e78c3234f48260
GeeMonday <gmonday@academic.rrc.ca>
2024-08-05 19:11:48 -0500

    Update asset files
    - Modified manifest.js in app/assets/config
    - Deleted application.css in app/assets/stylesheets
    - Added application.scss in app/assets/stylesheets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,8 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+// app/assets/config/manifest.js
+//= link application.scss
+//= link application.css
+//= link application.js
+ //= link controllers/application.js
+ //= link controllers/hello_controller.js
+ //= link controllers/index.js
+   //= link default_image.png
